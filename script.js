55aaf397b207a8e1cec7ffaaf5dd4907503c952f
Unnamed00000 <157989745+Unnamed00000@users.noreply.github.com>
2026-08-06 19:50:41 +0200

    Allow longer place and series values

--- script.js
+++ script.js
@@ -330,7 +330,7 @@ const I18N = {
 };
 
 const SHIFT_START = "06:00";
-const APP_VERSION = "1.3.0";
+const APP_VERSION = "1.3.1";
 const DEFAULT_LANGUAGE = "da";
 const LEGACY_STORAGE_KEY = "kpk-work-sheet";
 const STORAGE_PREFIX = "kpk-work-sheet:";
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kpk-arbejdsseddel-v29";
+const CACHE_NAME = "kpk-arbejdsseddel-v30";
 const APP_FILES = [
   "./",
   "./index.html",
