2e7dc6fbcc7fa17989436fb91b3c63cc0f65d769
Unnamed00000 <157989745+Unnamed00000@users.noreply.github.com>
2026-09-18 18:18:42 +0200

    Swap meeting button order

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "kpk-arbejdsseddel-v114";
+const CACHE_NAME = "kpk-arbejdsseddel-v115";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=1.4.75",
-  "./script.js?v=1.4.75",
+  "./styles.css?v=1.4.76",
+  "./script.js?v=1.4.76",
   "./manifest.webmanifest",
   "./assets/kpk-logo.svg",
   "./assets/kpk-logo.png",
